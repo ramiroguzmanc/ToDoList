a64d493e9e8259465af9fb740f39c1bcc3b08e73
Ramiro Guzmán C <ramiroguzmanc@gmail.com>
2021-08-25 19:37:07 -0500

    Agregando nuevos componentes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,31 @@
+import React from "react";
+import { CreateTodoButton } from "./Components/CreateTodoButton";
+import { TodoCounter } from "./Components/TodoCounter";
+import { TodoSearch } from "./Components/TodoSearch";
+import { TodoList } from "./Containers/TodoList";
+import { TodoItem } from "./Components/TodoItem";
 // import './App.css';
 
+const toDos = [
+  { text: "Cortar cebolla", completed: false },
+  { text: "Tomar el curso de Intro de React", completed: false },
+  { text: "Llorar con la llorona", completed: false },
+];
+
 function App() {
-  return <></>;
+  return (
+    <React.Fragment>
+      <TodoCounter />
+      <CreateTodoButton />
+      <TodoSearch />
+      <TodoList>
+        {toDos.map((todo, idx) => (
+          <TodoItem text={todo.text} key={idx} />
+        ))}
+      </TodoList>
+      <CreateTodoButton />
+    </React.Fragment>
+  );
 }
 
 export default App;
---
+++ src/Components/TodoSearch.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const TodoSearch = () => {
+  return (
+    <React.Fragment>
+      <input type="text" placeholder="Cebolla" />
+    </React.Fragment>
+  );
+};
+
+export { TodoSearch };
