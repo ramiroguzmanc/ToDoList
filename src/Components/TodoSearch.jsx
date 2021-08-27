4b229e5de1e534deb06e312c596981ac0c5039cb
Ramiro Guzmán C <ramiroguzmanc@gmail.com>
2021-08-26 22:23:13 -0500

    Borde al buscador y mayor padding vertical en el contenedor de tareas

--- src/Components/TodoSearch.jsx
+++ src/Components/TodoSearch.jsx
@@ -4,7 +4,7 @@ import "../styles/TodoSearch.css";
 const TodoSearch = () => {
   return (
     <div className="searchInputContainer">
-      <input type="text" placeholder="Cebolla" className="searchInput" />
+      <input type="text" placeholder="Buscar ToDo..." className="searchInput" />
     </div>
   );
 };
